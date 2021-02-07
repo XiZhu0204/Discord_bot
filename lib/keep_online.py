2928d9ff882e8f9b00a32bbc0e04b8c3b72f948b
Xi Zhu <x295zhu@uwaterloo.ca>
2021-02-07 09:04:14 +0000

    updated dad joke detection

--- lib/keep_online.py
+++ lib/keep_online.py
@@ -5,11 +5,14 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+  # this is called every 5 min
+  return "Bot still online"
+
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
+
 def keep_online():
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
--- main.py
+++ main.py
@@ -26,49 +26,59 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        if message.author == client.user:
-            return
+      if message.author == client.user:
+          return
 
-        if "I am" in message.content:
-            end_piece = message.content.split('I am')[-1]
-            await message.channel.send(f'Hello{end_piece}! I am dad!')
-        elif message.content.startswith("!materials"):
-            weekday = DAYS_OF_WEEK[DATE.now(working_tz).weekday()]
-            if weekday == "Sunday":
-                await message.channel.send("It's Sunday you cringe kid.")
-            else:
-                list_of_mats = get_farmable_mats(weekday)
-                response = "You can waste your time today farming:\n"
-                for entry in list_of_mats:
-                    response += entry + "\n"
-                await message.channel.send(response)
-        elif message.content.startswith("!when "):
-            mat = message.content.split('!when ')[-1]
-            mat = mat.capitalize()
-            list_of_days = find_farmable_day(mat)
-            if list_of_days:
-                # non empty array, indicating that retrieval was successful
-                response = f"Greetings you gacha cringe, {mat} can be farmed on:\n"
-                for entry in list_of_days:
-                    response += entry + "\n"
-                response += "Sunday"
-                await message.channel.send(response)
-            else:
-                await message.channel.send("Spell correctly xd")
+      if message.content.lower().startswith("i am ") or " i am " in message.content.lower():
+        end_piece = message.content.lower().split('i am')[-1]
+        await message.channel.send(f'Hello{end_piece}! I am dad!')
+
+      elif message.content.lower().startswith("im ") or " im " in message.content.lower():
+        end_piece = message.content.lower().split('im')[-1]
+        await message.channel.send(f'Hello{end_piece}! I am dad!')
+
+      elif message.content.lower().startswith("i'm ") or " i'm " in message.content.lower():
+        end_piece = message.content.lower().split("i'm")[-1]
+        await message.channel.send(f'Hello{end_piece}! I am dad!')
+
+      elif message.content.startswith("!materials"):
+        weekday = DAYS_OF_WEEK[DATE.now(working_tz).weekday()]
+        if weekday == "Sunday":
+          await message.channel.send("It's Sunday you cringe kid.")
+        else:
+          list_of_mats = get_farmable_mats(weekday)
+          response = "You can waste your time today farming:\n"
+          for entry in list_of_mats:
+            response += entry + "\n"
+          await message.channel.send(response)
+          
+      elif message.content.startswith("!when "):
+        mat = message.content.split('!when ')[-1]
+        mat = mat.capitalize()
+        list_of_days = find_farmable_day(mat)
+        if list_of_days:
+          # non empty array, indicating that retrieval was successful
+          response = f"Greetings you gacha cringe, {mat} can be farmed on:\n"
+          for entry in list_of_days:
+            response += entry + "\n"
+          response += "Sunday"
+          await message.channel.send(response)
+        else:
+          await message.channel.send("Spell correctly xd")
 
 
 def get_farmable_mats(day):
-    return FARMABLE_MATS_BY_WEEKDAY[day]
+  return FARMABLE_MATS_BY_WEEKDAY[day]
 
 
 def find_farmable_day(mat):
-    farmable_days = []
+  farmable_days = []
 
-    for day in FARMABLE_MATS_BY_WEEKDAY:
-        if mat in FARMABLE_MATS_BY_WEEKDAY[day]:
-            farmable_days.append(day)
+  for day in FARMABLE_MATS_BY_WEEKDAY:
+    if mat in FARMABLE_MATS_BY_WEEKDAY[day]:
+      farmable_days.append(day)
 
-    return farmable_days
+  return farmable_days
 
 
 client = MyClient()
