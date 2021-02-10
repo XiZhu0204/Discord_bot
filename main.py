cb3d7570bf6480b0d8a645f7d793d4f7ec8c88ec
Xi Zhu <x295zhu@uwaterloo.ca>
2021-02-10 01:35:54 +0000

    Refactored commands to be independent of on_message event

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import discord
+from discord.ext import commands
 import os
 
 import lib.data_accessors as d_access
 import lib.resin_tracker as resin
 from lib.keep_online import keep_online
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = "!")
 
-@client.event
+@bot.event
 async def on_ready():
   resin.increment_resin.start()
-  print('Logged on as {0}!'.format(client.user))
+  print('Logged on as {0}!'.format(bot.user))
 
 
-@client.event
+@bot.event
 async def on_message(message):
-  if message.author == client.user:
+  if message.author == bot.user:
     return
 
   if message.content.lower().startswith("i am ") or " i am " in message.content.lower():
@@ -30,30 +30,43 @@ async def on_message(message):
     end_piece = message.content.lower().split("i'm")[-1]
     await message.channel.send(f'Hello{end_piece}! I am dad!')
 
-  elif message.content.startswith("!materials"):
-    weekday = d_access.get_weekday()
-    if weekday == "Sunday":
-      await message.channel.send("It's Sunday you cringe kid.")
-    else:
-      list_of_mats = d_access.get_farmable_mats(weekday)
-      response = "You can waste your time today farming:\n"
-      for entry in list_of_mats:
-        response += entry + "\n"
-      await message.channel.send(response)
-          
-  elif message.content.startswith("!when "):
-    mat = message.content.split('!when ')[-1]
-    mat = mat.capitalize()
-    list_of_days = d_access.find_farmable_day(mat)
-    if list_of_days:
-      # non empty array, indicating that retrieval was successful
-      response = f"Greetings you gacha cringe, {mat} can be farmed on:\n"
-      for entry in list_of_days:
-        response += entry + "\n"
-      response += "Sunday"
-      await message.channel.send(response)
-    else:
-      await message.channel.send("Spell correctly xd. The materials list is \n{}".format(d_access.get_material_list()))
+  await bot.process_commands(message)
+
+
+@bot.command()
+async def materials(ctx):
+  weekday = d_access.get_weekday()
+  if weekday == "Sunday":
+    await ctx.send("```\nIt's Sunday you cringe kid.\n```")
+  else:
+    list_of_mats = d_access.get_farmable_mats(weekday)
+    response = "```\nYou can waste your time today farming:\n"
+    for entry in list_of_mats:
+      response += entry + "\n"
+    response += "\n```"
+    await ctx.send(response)
+
+
+@bot.command()
+async def when(ctx, arg):
+  mat = arg.capitalize()
+  list_of_days = d_access.find_farmable_day(mat)
+  if list_of_days:
+    # non empty array, indicating that retrieval was successful
+    response = f"```\nGreetings you gacha cringe, {mat} can be farmed on:\n"
+    for entry in list_of_days:
+      response += entry + "\n"
+    response += "Sunday\n```"
+    await ctx.send(response)
+  else:
+    await ctx.send("Spell correctly xd. The materials list is \n{}".format(d_access.get_material_list()))
+
+
+##### Work in Progress
+'''
+@bot.command()
+async def resin(ctx):
+  user = ctx.author.name
 
   elif message.content.startswith("!resin set "):
     user = message.author.name
@@ -73,7 +86,7 @@ async def on_message(message):
       await message.channel.send("{} has about {} resin".format(user, amount))
     else:
       await message.channel.send("{} is not in the database".format(user))
-
+'''
 
 keep_online()
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
