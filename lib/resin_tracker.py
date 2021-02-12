d5547acf101320e374cea8501d6f479aa6ab35fb
Xi Zhu <x295zhu@uwaterloo.ca>
2021-02-12 02:35:04 +0000

    Resin counts up after correct time

--- lib/resin_tracker.py
+++ lib/resin_tracker.py
@@ -31,7 +31,7 @@ def noti_off(user):
   user_data.pop("noti", None)
   db[user] = user_data
 
-@tasks.loop(seconds = 3.0)
+@tasks.loop(minutes = 8.0)
 async def increment_resin(bot):
   for user in db.keys():
     user_data = db[user]
